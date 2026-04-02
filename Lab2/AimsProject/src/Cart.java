210cf4d96e5e84a930b4d5560c40325b60ec9432
Trung01122003 <trunghoanggv@gmail.com>
2026-04-02 10:56:10 +0700

    source code

---
+++ Lab2/AimsProject/src/Cart.java
@@ -0,0 +1,40 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                // Shift elements to the left to fill the gap
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                return;
+            }
+        }
+        System.out.println("The disc was not found in the cart");
+    }
+
+    public float totalCost() {
+        float sum = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            sum += itemsOrdered[i].getCost();
+        }
+        return sum;
+    }
+}
