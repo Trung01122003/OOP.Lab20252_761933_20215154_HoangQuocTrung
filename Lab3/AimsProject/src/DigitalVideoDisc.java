f980221df7d438d317dc525bc2e432afc97f9e86
Trung01122003 <trunghoanggv@gmail.com>
2026-04-17 10:00:11 +0700

    Feature: search cart

--- Lab3/AimsProject/src/DigitalVideoDisc.java
+++ Lab3/AimsProject/src/DigitalVideoDisc.java
@@ -76,4 +76,8 @@ public class DigitalVideoDisc {
     public String toString() {
         return "DVD - " + this.title + " - " + this.category + " - " + this.director + " - " + this.length + ": " + this.cost + " $";
     }
+
+    public boolean isMatch(String title) {
+        return this.title != null && this.title.toLowerCase().contains(title.toLowerCase());
+    }
 }
